1a328bad7a36858381c9ff1041be5f244ece28e7
Asocia <akkayas17@itu.edu.tr>
2021-03-18 16:28:16 +0300

    Wait between 0-3 seconds before returning response

--- django/api/views.py
+++ django/api/views.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+from random import random
+from time import sleep
 
 from rest_framework import status
 from rest_framework.decorators import api_view
@@ -6,6 +9,12 @@ from rest_framework.response import Response
 
 def handle_request(request, format=None):
     """ Return success for all requests """
+    sleep_time = random() * 3
+    sleep(sleep_time)
+    timestamp = int(datetime.now().timestamp())
+    log_str = f'{request.method},{sleep_time*1000:.0f},{timestamp}'
+    with open('api.log', 'a+') as f:
+        print(log_str, file=f)
 
     return Response(status=status.HTTP_200_OK)
 
