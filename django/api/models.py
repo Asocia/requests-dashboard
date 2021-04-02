bc9a1db5a8e328aeba23c0199a6485086b521279
Asocia <akkayas17@itu.edu.tr>
2021-04-02 14:57:52 +0300

    Create model for storing request info in db

--- django/api/models.py
+++ django/api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Request(models.Model):
+    type = models.CharField(max_length=6)
+    response_time = models.PositiveSmallIntegerField()
+    datetime = models.DateTimeField()
+
+    def __str__(self):
+        return f'{self.type} - {self.response_time} ms'
