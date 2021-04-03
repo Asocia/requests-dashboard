f90081f639ffb855e1a826dcd3aed89d1647de98
Asocia <akkayas17@itu.edu.tr>
2021-04-03 23:39:42 +0300

    Add a new helper field to Request model

--- django/api/models.py
+++ django/api/models.py
@@ -5,6 +5,7 @@ class Request(models.Model):
     type = models.CharField(max_length=6)
     response_time = models.PositiveSmallIntegerField()
     datetime = models.DateTimeField()
+    shown_in_graph = models.BooleanField(default=False)
 
     def __str__(self):
-        return f'{self.type} - {self.response_time} ms'
+        return f'{self.type} - {self.response_time} ms - {self.datetime.strftime("%H:%M")}'
